fc35d6379c9b5c8a95f7c1c2b28f7d59e26b55d9
semicolon2 <benfallan12@gmail.com>
2016-03-19 14:06:57 -0600

    initial commit

---
+++ application/files/cache/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/2ddb27c5cdf0b672/745d4c64665be841/2702dcf7117790d7/e7fefe28bf130c11.php
@@ -0,0 +1,16 @@
+<?php 
+/* Cachekey: cache/stash_default/doctrine/doctrinenamespacecachekey[dc2_26eabe359c11873ff1120fb1bd372f63_]/ */
+/* Type: array */
+
+
+
+$loaded = true;
+$expiration = 1458639330;
+
+$data = array();
+
+/* Child Type: integer */
+$data['return'] = 1;
+
+/* Child Type: integer */
+$data['createdOn'] = 1458238856;
